cb73d642bc9e2e62071457ba7a5984d485d36d8c
Shubham <rana2001shubham@gmail.com>
2021-02-09 23:58:35 +0530

    adding day4-5

---
+++ Day5/new.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+
+struct student{
+    char name[20];
+    int roll;
+    int phone;
+    int classM;
+}
+int main(){
+
+    student x;
+    cout<<"Enter Name";cin>>x.name;
+    cout<<"Enter roll";cin>>x.roll;
+    cout<<"Enter Phone";cin>>x.phone;
+    cout<<"Enter Phone";cin>>x.classM;
+}
